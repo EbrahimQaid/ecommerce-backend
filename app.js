df8afdea4b4b3ebb6f445cb89b2a04e5f4f66079
Ebrahim Qaid <ebrahemsife48@gmail.com>
2026-08-12 00:13:35 +0300

    chore: create uploads directory

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 require("./db.js");
 const app = express();
+
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
