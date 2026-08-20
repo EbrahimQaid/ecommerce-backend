aa7b09cd35638afc216e9a26f4aefe290d118d00
Ebrahim Qaid <ebrahemsife48@gmail.com>
2026-08-20 21:11:05 +0300

    Change file name in middleware dir

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,7 +3,7 @@ const productsController = require("../controller/products.controller.js");
 const verifyToken = require("../middleware/verifyToken.js");
 const userRoles = require("../utils/userRoles.js");
 const allowedTo = require("../middleware/allowedTo.js");
-const { upload } = require("../middleware/uniqeAvatarName.js");
+const { upload } = require("../middleware/upload.js");
 const router = express.Router();
 
 router
--- routes/users.route.js
+++ routes/users.route.js
@@ -3,7 +3,7 @@ const usersController = require("../controller/users.controller.js");
 const verifyToken = require("../middleware/verifyToken.js");
 const allowedTo = require("../middleware/allowedTo.js");
 const userRoles = require("../utils/userRoles.js");
-const { upload } = require("../middleware/uniqeAvatarName.js");
+const { upload } = require("../middleware/upload.js");
 const router = express.Router();
 router
   .route("/")
